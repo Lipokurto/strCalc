99435c1b6bc5ead415e699894806bb01ebe0b23f
Lipokurto <lighting13@yandex.ru>
2021-01-21 23:04:23 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,45 +9,42 @@ class App extends Component {
     super(props)
     this.state = {
       value: '',
-      price1: (0).toFixed(2),
-      price2: (0).toFixed(2),
-      price3: (0).toFixed(2),
-      price4: (0).toFixed(2),
-      price5: (0).toFixed(2),
-    };
-
+      price:[0,0,0,0,0],
+      size:['100*25*6000','150*25*6000','50*50*6000','50*100*6000','100*100*6000'],
+      cauntIn:[66,44,66,33,16]
+    };  
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
+    
     let val = event.nativeEvent.data
-    console.log(isNaN(' '))
     if(isNaN(parseInt(val))) {
       this.setState({value: this.state.value})
-      this.setState({price1: (this.state.value / 66).toFixed(2)})
-      this.setState({price2: (this.state.value / 44).toFixed(2)})
-      this.setState({price3: (this.state.value / 66).toFixed(2)})
-      this.setState({price4: (this.state.value / 33).toFixed(2)})
-      this.setState({price5: (this.state.value / 16).toFixed(2)})
+      let d = this.state.price.slice()
+      for (let i=0; i<this.state.price.length;i++) {
+        d[i] = (this.state.value / this.state.cauntIn[i]).toFixed(2)
+        this.setState({price:d})
+      }
     } else {
       this.setState({value: event.target.value})
-      this.setState({price1: (event.target.value / 66).toFixed(2)})
-      this.setState({price2: (event.target.value / 44).toFixed(2)})
-      this.setState({price3: (event.target.value / 66).toFixed(2)})
-      this.setState({price4: (event.target.value / 33).toFixed(2)})
-      this.setState({price5: (event.target.value / 16).toFixed(2)})
+      let d = this.state.price.slice()
+      for (let i=0; i<this.state.price.length;i++) {
+        d[i]=(event.target.value / this.state.cauntIn[i]).toFixed(2)
+        this.setState({price:d})
+      }
+      debugger
     }
   }
- 
-  render() {
+  render() { 
     return (
       <div className='calc-wrapper'>
         <div className='app'>
             <InputPrice value={this.state.value} onChange={this.handleChange}/>
-            <PriceStick result={this.state.price1}>100*25*6000</PriceStick>
-            <PriceStick result={this.state.price2}>150*25*6000</PriceStick>
-            <PriceStick result={this.state.price3}>50*50*6000</PriceStick>
-            <PriceStick result={this.state.price4}>50*100*6000</PriceStick>
-            <PriceStick result={this.state.price5}>100*100*6000</PriceStick>
+              {this.woodParPrice = this.state.price.map((price,index)=> 
+                <PriceStick key={index} result={price}>
+                  {this.state.size[index]}
+                  </PriceStick>
+                  )}
             <Buttons onClickEre={()=>
             this.setState({value:'',
             price1: (0).toFixed(2),
